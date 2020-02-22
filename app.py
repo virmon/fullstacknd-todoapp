32c3467f6b65787b99975230f4ed25db8d0f5315
virmon <virmontumulak@gmail.com>
2020-02-22 11:41:40 -0600

    added update todo item

--- app.py
+++ app.py
@@ -30,6 +30,20 @@ def create_todo():
         'description': todo.description
     })
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        # print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
 @app.route('/')
 def index():
     return render_template('index.html', data=Todo.query.all())
